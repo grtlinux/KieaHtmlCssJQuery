04819c5d61b7ecf1b5a0bb4adbf5bdcd519b43d2
grtlinux <grtlinux@gmail.com>
2018-11-09 03:07:39 +0900

    create 1st

---
+++ test02/javascript-jquery-01/src/AppMain.java
@@ -0,0 +1,9 @@
+
+public class AppMain {
+
+	private static final boolean flag = true;
+
+	public static void main(String[] args) throws Exception {
+		if (flag) System.out.println(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+	}
+}
